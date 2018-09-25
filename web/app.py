ff0f01f5bf0ce3b31142304fd078a524a7e0e0ca
Rafael R. Moser <rrmoser@gmail.com>
2018-09-25 01:00:39 -0300

    - applied pycharm codestyle; - requirements.txt, just because;

--- web/app.py
+++ web/app.py
@@ -3,10 +3,12 @@ from flask import render_template
 
 app = Flask(__name__, template_folder="./", static_folder='./', static_url_path='')
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-  app.debug=True
-  app.run(host='0.0.0.0')
+    app.debug = True
+    app.run(host='0.0.0.0')
